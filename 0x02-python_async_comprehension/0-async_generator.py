08bdd6b6469ecd7efd2ed8671f663b1b797770c6
Berekto <bereketalebachew87@gmail.com>
2023-05-03 19:40:58 +0300

    async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Asynchronously wait 1 second, then yield a random number between 0 and 10"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """asynchronously wait 1 second, then
        yield a random number between 0 and 10
     """
