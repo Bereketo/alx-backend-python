92f800f0a8c324c4c29a393809393c64d0f176f6
Berekto <bereketalebachew87@gmail.com>
2023-05-21 22:41:52 +0300

    Parameterized unit test

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Parametrized Test
+"""
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Unittest for access_nested_map function
+    """
+    @parameterized.expand([
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2)
+            ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """tests if the fucntion works as expected"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
